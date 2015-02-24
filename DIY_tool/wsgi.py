f68b2393c139849dc1ed15c0f0baeaea34b51dd7
add pro photo form <moon@moonui-MacBook-Pro.local>
2015-02-24 19:34:33 +0900

    "setting static on S3 finish"

--- DIY_tool/wsgi.py
+++ DIY_tool/wsgi.py
@@ -6,9 +6,24 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+local = True
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DIY_tool.settings")
+if local:
+    import os
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DIY_tool.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+else :
+    import os
+    import sys
+
+    ##setting uer virtualenv##
+    activate_this = '/home/ubuntu/.virtualenv/diy_tec/bin/activate_this.py'
+    execfile(activate_this, dict(__file__=activate_this))
+    ##--##
+    sys.path.append("/opt/diyroot")
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DIY_tool.settings")
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
