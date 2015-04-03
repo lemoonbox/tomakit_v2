ca01d9d3ea7008bce7488f31fc21e1b60abc9ffb
add pro photo form <moon@moonui-MacBook-Pro.local>
2015-04-03 11:34:00 +0900

    "before batch"

--- app_idealine/views.py
+++ app_idealine/views.py
@@ -38,6 +38,32 @@ def idealine(request):
             'HTTP_HOST':host,
         })
 
+def idealine_page(request, page_num=1):
+
+    ctx = Context({
+        'error':None
+    })
+    error = False
+
+    if request.method=="GET":
+        _class_line=ClassPost.objects.order_by('-id')[:9]
+        _kit_line=Kit_Post.objects.order_by('-id')[:9]
+
+        itea_line=[i for i in itertools.chain(*itertools.izip_longest(
+            _class_line, _kit_line)) if i is not None]
+
+        host= request.META['HTTP_HOST']
+
+
+    return render(request, 'app_idealine/idealine.html',
+        {
+            'lineitem':itea_line,
+            'HTTP_HOST':host,
+        })
+
+
+
+
 def categoryline(request, category):
     ctx = Context({
         'error':None
