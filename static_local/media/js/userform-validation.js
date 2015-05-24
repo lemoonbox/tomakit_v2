4257f018c564e68ee47942df3cc399453627cc83
Mina Hyeon <hellostranger.mm@gmail.com>
2015-05-25 00:49:20 +0900

    "sign-up form validation error fixed"

--- static_local/media/js/userform-validation.js
+++ static_local/media/js/userform-validation.js
@@ -1,5 +1,5 @@
 // <![CDATA[
-jQuery(function ($) {
+jQuery( function ($) {
   var re_id_email = /^[0-9a-zA-Z]([\-.\w]*[0-9a-zA-Z\-_+])*@([0-9a-zA-Z][\-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9}$/; //email
   var re_nick_name = /^[가-힣a-zA-Z]{2,15}$/; //아이디 검사식
 //  var re_id_password, re_id_password_confirm = /(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,16}$/; //pw
@@ -7,7 +7,8 @@ jQuery(function ($) {
   //                 var re_url = /^(https?:\/\)?([a-z\d\.-]+)\/([a-z\.]{2,6})([\/\w\.-]*)*\/?$/; //url
   //                 var re_tel = /^[0-9]{8,11}$/; //tel
 
-  var form = $('.form-signin'),
+  var
+  form = $('.form-signin'),
       nick_name = $('#nick_name'),
       id_password = $('#id_password'),
       id_password_confirm = $('#id_password_confirm'),
@@ -16,7 +17,7 @@ jQuery(function ($) {
   //            tel = $('#tel');
 
 
-//  form.submit(function () {
+  form.submit(function () {
 //    if (form.val() === "") {
 //      alert('모든 항목을 정확히 기입해주세요.');
 //      return false;
@@ -26,11 +27,15 @@ jQuery(function ($) {
 //      alert('@ 올바른 형식의 이메일이 아닌 것 같아요.');
 //      id_email.focus();
 //      return false;
-//    } else if(re_id_password.test(id_password.val()) != true) {
-//      alert('비밀번호는 6자리 이상 18자리 이하로 입력해주세요.');
-//      id_password.focus();
-//      return false;
-//    } else if(re_nick_name.test(nick_name.val()) != true) {
+//    } else 
+  
+  if(id_password.val().length < 6) {
+      alert('비밀번호는 6자리 이상 18자리 이하로 입력해주세요.');
+      id_password.focus();
+      return false;
+    }
+  });
+//    else if(re_nick_name.test(nick_name.val()) != true) {
 //      alert('닉네임이 올바르지 않습니다.');
 //      nick_name.focus();
 //      return false;
@@ -60,7 +65,7 @@ jQuery(function ($) {
       s.text('');
     } else if (nick_name.val().length ==1 ) {
       s.text('최소 2자 이상 입력해주세요.');
-    } else if (nick_name.val().length > 10) {
+    } else if (nick_name.val().length > 15) {
       s.text('15자 이내로 입력해주세요.');
     } else if (re_nick_name.test(nick_name.val()) !== true) {
       s.text('한글 혹은 영어만 사용할 수 있습니다.');
