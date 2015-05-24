b589bc8f090ca8702e99d6d4deffc6b0eddffa30
add pro photo form <moon@moonui-MacBook-Pro.local>
2015-05-24 22:40:05 +0900

    ff

--- userapp/models.py
+++ userapp/models.py
@@ -57,3 +57,15 @@ class PasswordResetKeys(models.Model):
             ret[0].delete()
 
         return ret[0]
+
+class SellerProfile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    email = models.EmailField(max_length=100, unique=True, null=False, blank=False)
+    nick_name = models.CharField(max_length=20, null=False, blank=False)
+    pro_photo = models.ImageField(upload_to=upload_to)
+    mobile = models.CharField(max_length=30, unique=True, null=True)
+    address = models.CharField(max_length=255, null=True)
+    email_confirm = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now_add=True, auto_now=True)
