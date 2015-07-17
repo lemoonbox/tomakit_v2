540b6ec7d30423e1940ec84416c7c89c03430e44
add pro photo form <moon@moonui-MacBook-Pro.local>
2015-07-17 16:48:12 +0900

    "add sign and send mail"

---
+++ utils/tasks.py
@@ -0,0 +1,41 @@
+#coding: utf-8
+from __future__ import absolute_import
+__author__ = 'moon'
+from djcelery import celery
+from django.core.mail import send_mail
+
+@celery.task(name = 'tasks.send_mail')
+def sendmail(cont, recipient):
+    send_mail(u'안녕하세요! 토마킷입니다. 정식 사용을 승인해주세요.', "",
+            'makerecipe@gmail.com', recipient, fail_silently=False,
+            html_message=cont)
+
+@celery.task(name = 'tasks.send_pwchang_mail')
+def send_pwchang_mail(cont, recipient):
+    send_mail(u'다음 링크를 클릭하면 패스워드를 바꾸실 수 있습니다.', "",
+            'makerecipe@gmail.com', recipient, fail_silently=False,
+            html_message=cont)
+
+@celery.task(name = 'tasks.contact_mail')
+def contact_mail(cont, recipient):
+    send_mail(u'고객님이 보내신 메일입니다.', "",
+            'makerecipe@gmail.com', recipient, fail_silently=False,
+            html_message=cont)
+
+
+# @celery.task(name = 'tasks.rabbitmqtest')
+# def rabbitmqtest():
+#     print "task test"
+#
+# from celery import shared_task
+# @shared_task
+# def add(x, y):
+#     return x + y
+#
+# @shared_task
+# def mul(x, y):
+#     return x * y
+#
+# @shared_task
+# def xsum(numbers):
+#     return sum(numbers)
---
+++ utils/utils.py
@@ -0,0 +1,190 @@
+__author__ = 'moon'
+from DIY_tool import settings
+
+
+def handle_uploaded_image(i, x, y):
+    import StringIO
+    from PIL import Image, ImageOps
+    import os
+    from django.core.files.images import ImageFile
+    from django.core.files.uploadedfile import  InMemoryUploadedFile
+    import uuid
+
+
+    IMAGE_TYPE=i.content_type
+
+    #PIL_TYPE use to save disk, FILE_EXTENSTION use to make the path
+    print
+    if IMAGE_TYPE =='image/jpeg':
+         PIL_TYPE = 'image/jpg'
+         PIL_SAVE = 'JPEG'
+         FILE_EXTENSION = 'jpg'
+    elif IMAGE_TYPE=='image/png':
+        PIL_TYPE = 'image/png'
+        PIL_SAVE = 'PNG'
+        FILE_EXTENSION = 'png'
+    else :
+        raise Exception('Image-type-error', "It's not jpeg of PNG")
+
+
+    #read image from InMemoryUploadedFile
+    image_str = ""
+    for c in i.chunks():
+        image_str +=c
+
+    #create PIL image instance
+    imagefile = StringIO.StringIO(image_str)
+    image = Image.open(imagefile)
+
+    # if not RGB, convert
+    if image.mode not in ("L", "RGB"):
+        image = image.convert("RGB")
+
+    width, height = image.size
+
+    max_width = x
+    max_height = y
+
+    target_ratio = float(max_width)/float(max_height)
+    ratio = float(width)/float(height)
+
+    target_width = 0
+    target_height = 0
+
+    if (ratio>target_ratio):
+        target_width = max_width
+        target_height = int(target_width/ratio)
+    else:
+        target_height = max_height
+        target_width = int(ratio * target_height)
+
+    #resize(doing a thumb)
+    if target_width < width or target_height < height:
+        image = image.resize((target_width, target_height), Image.ANTIALIAS)
+
+
+
+    #re-initialize imageFile and set a hash (unique filename)
+    filename = str(uuid.uuid1())+"."+FILE_EXTENSION
+
+    #PIL instance(image) change to imagefile
+    #return FILE instance after just resize not save
+    thumnail_io= StringIO.StringIO()
+    image.save(thumnail_io, format=PIL_SAVE)
+    thumnail_io.seek(0)
+
+    content = InMemoryUploadedFile(thumnail_io, None, filename, PIL_TYPE,
+                                   thumnail_io.len, None)
+
+    """
+    #save to disk
+    #save first and read again style
+    imagefile = open(os.path.join(settings.MEDIA_ROOT, filename), 'w')
+    image.save(imagefile, 'JPEG', quality=90)
+    image_file = open(os.path.join(settings.MEDIA_ROOT, filename), 'r')
+    content = ImageFile(image_file)
+
+    """
+
+    return (filename, content)
+
+
+def handle_uploaded_image2(i, x, y):
+    import StringIO
+    from PIL import Image, ImageOps
+    import os
+    from django.core.files.images import ImageFile
+    from django.core.files.uploadedfile import  InMemoryUploadedFile
+    import uuid
+
+
+    IMAGE_TYPE=i.content_type
+
+    #PIL_TYPE use to save disk, FILE_EXTENSTION use to make the path
+    print
+    if IMAGE_TYPE =='image/jpeg':
+         PIL_TYPE = 'image/jpg'
+         PIL_SAVE = 'JPEG'
+         FILE_EXTENSION = 'jpg'
+    elif IMAGE_TYPE=='image/png':
+        PIL_TYPE = 'image/png'
+        PIL_SAVE = 'PNG'
+        FILE_EXTENSION = 'png'
+    else :
+        raise Exception('Image-type-error', "It's not jpeg of PNG")
+
+
+    #read image from InMemoryUploadedFile
+    image_str = ""
+    for c in i.chunks():
+        image_str +=c
+
+    #create PIL image instance
+    imagefile = StringIO.StringIO(image_str)
+    image = Image.open(imagefile)
+
+    # if not RGB, convert
+    if image.mode not in ("L", "RGB"):
+        image = image.convert("RGB")
+
+    #define file output dimensions
+    x = x
+    y = y
+
+    # get orginal image ratio
+    img_ratio = float(image.size[0])/image.size[1]
+
+    #resize but constrain prorpottions?
+    if x==0.0:
+        x = y* img_ratio
+    elif y==0.0:
+        y = x/img_ratio
+
+    #output file ratio
+    resize_ratio = float(x)/y
+    x = int(x); y = int(y)
+
+    #get output with and height to do the first crotp
+
+    if(img_ratio > resize_ratio):
+        output_width = x*image.size[1]/y
+        output_height = image.size[1]
+        originX = image.size[0]/ 2 - output_width /2
+        originY = 0
+    else :
+        output_width = image.size[0]
+        output_height = y* image.size[1]/x
+        originX = 0
+        originY = image.size[1]/ 2 - output_width /2
+
+    #crop
+    cropBox = (originX, originY, originX + output_width, originY+output_height)
+    image = image.crop(cropBox)
+
+    #resize(doing a thumb)
+    image.thumbnail([x,y], Image.ANTIALIAS)
+
+    #re-initialize imageFile and set a hash (unique filename)
+    imagefile = StringIO.StringIO()
+    filename = str(uuid.uuid1())+"."+FILE_EXTENSION
+
+    #PIL instance(image) change to imagefile
+    #return FILE instance after just resize not save
+    thumnail_io= StringIO.StringIO()
+    image.save(thumnail_io, format=PIL_SAVE)
+    thumnail_io.seek(0)
+
+    content = InMemoryUploadedFile(thumnail_io, None, filename, PIL_TYPE,
+                                   thumnail_io.len, None)
+
+    """
+    #save to disk
+    #save first and read again style
+    imagefile = open(os.path.join(settings.MEDIA_ROOT, filename), 'w')
+    image.save(imagefile, 'JPEG', quality=90)
+    image_file = open(os.path.join(settings.MEDIA_ROOT, filename), 'r')
+    content = ImageFile(image_file)
+
+    """
+
+    return (filename, content)
