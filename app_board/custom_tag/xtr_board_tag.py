1076a7a9d640d95fcf8b5c24bf76dd6123a081c1
add pro photo form <moon@moonui-MacBook-Pro.local>
2015-08-11 17:15:06 +0900

    "xtr_tage"

--- app_board/custom_tag/xtr_board_tag.py
+++ app_board/custom_tag/xtr_board_tag.py
@@ -8,4 +8,33 @@ register = template.Library()
 
 @register.filter()
 def cate2han(valu, argu):
-    if valu == ""
+    if valu == "leather":
+        return
+    elif valu =="calligraphy":
+        return
+    elif valu =="flower":
+        return
+    elif valu =="dyeing":
+        return
+    elif valu =="pottery":
+        return
+    elif valu =="home-living":
+        return
+    elif valu =="sewing":
+        return
+    elif valu =="etc":
+        return
+    elif valu =="woodwork":
+        return
+    elif valu =="art":
+        return
+    elif valu =="toreutics":
+        return
+    elif valu =="knot":
+        return
+    elif valu =="knit":
+        return
+    elif valu =="bake":
+        return
+    elif valu =="cosmetics":
+        return
