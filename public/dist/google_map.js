8d9f55db8187f41cf2d5c28f172e908ea3c4fadf
Mina Hyeon <hellostranger.mm@gmail.com>
2015-10-08 17:45:20 +0900

    carousel a opacity

--- public/dist/google_map.js
+++ public/dist/google_map.js
@@ -66,7 +66,25 @@ function geolocate() {
             autocomplete.setBounds(circle.getBounds());
         });
     }
+}function initMap() {
+    var myLatLng = {lat: -25.363, lng: 131.044};
+
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        scrollwheel: false,
+        zoom: 4
+    });
+
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+        map: map,
+        position: myLatLng,
+        title: 'Hello World!'
+    });
 }
+
+
 function initialize() {
     var mapOptions = {
         center: {lat: -33.8688, lng: 151.2195},
