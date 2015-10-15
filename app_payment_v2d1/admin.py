12f7dcbfe2254a004b723c67c474f89e537e99a3
add pro photo form <moon@moonui-MacBook-Pro.local>
2015-10-15 15:56:58 +0900

    "before sink"

--- app_payment_v2d1/admin.py
+++ app_payment_v2d1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from app_payment_v2d1.models import PrePayment
 # Register your models here.
+class PrePaymentAdmin(admin.ModelAdmin):
+
+    list_display=('user','merchant_uid', 'pay_method','created_at','updated_at')
+    fieldsets=[("payInfo",{'fields':['user','classtype', 'merchant_uid',
+                                        'pay_method','amount', 'pay_name',
+                                        'buyer_name', 'buyer_email','buyer_mobli',
+                                        'want_day']})]
+
+admin.site.register(PrePayment, PrePaymentAdmin)
--- app_payment_v2d1/models.py
+++ app_payment_v2d1/models.py
@@ -16,3 +16,10 @@ class PrePayment(models.Model):
     buyer_email=models.CharField(max_length=100)
     buyer_mobli=models.CharField(max_length=100)
     want_day=models.DateField(null=False, default=datetime.date.today())
+
+    is_active=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True, auto_now=False, default=datetime.datetime.now())
+    updated_at=models.DateTimeField(auto_now_add=True, auto_now=True, default=datetime.datetime.now())
+
+    def __unicode__(self):
+        return  u'%s %s' % (self.id, self.merchant_uid)
