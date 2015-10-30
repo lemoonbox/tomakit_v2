3d2144b47b0efcf70076cd7987d51b49745aee73
Mina Hyeon <hellostranger.mm@gmail.com>
2015-10-30 22:34:02 +0900

    IE placeholder support

---
+++ static_local/media/js/tmk-selecter.js
@@ -0,0 +1,78 @@
+/**
+ * Created by jaewon on 15. 8. 1..
+ */
+function btnClick(selecterSelected) {
+    var $select = $(selecterSelected).parent();
+    var options = $select.find('.selecter-options').show();
+    $('.selecter').css("z-index", "0");
+    /**
+     *     Mina added 4 lines below on 15.9.22
+     */
+    $select.css("z-index", "150");
+    $('.selecter-options').mouseleave(function() {
+        $(this).hide();
+    });
+}
+
+
+function selecting(option, id) {
+    var $selecterOptions = $(option).parent();
+
+    var motionType = $selecterOptions.attr('data-value');
+    var $selecter = $selecterOptions.parent();
+    $selecter.find('.selecter-selected').html($(option).html());
+    $selecterOptions.hide();
+    $selecter.find('.selecter-item').removeClass("selected");
+    $(option).addClass("selected");
+    var formGroup = $selecterOptions.closest(".wgt-form-group");
+    var $selecterHiddenInput = $(formGroup).nextAll().find('input[type=hidden]').eq(0);
+    if(id == ''){
+        id = $(option).attr('data-value');
+        motionType = "00";
+        $(formGroup).nextAll('.wgt-selecter').remove();
+        $selecterHiddenInput.attr('data-value', "1");
+    }
+
+    $selecterHiddenInput.val(id);
+    $selecter.find('select').val(id);
+
+
+    $selecterOptions.hide();
+    $selecterOptions.parent().css("z-index", "0");
+}
+$( "#category" )
+    .change(function() {
+        var str = "";
+        $( "option:selected", this ).each(function() {
+            str += $( this ).text() + " ";
+        });
+        $( ".category-selected").text( str );
+    })
+    .trigger( "change" );
+$( "#state" )
+    .change(function() {
+        var str = "";
+        $( "option:selected", this ).each(function() {
+            str += $( this ).text() + " ";
+        });
+        $( ".state-selected").text( str );
+    })
+    .trigger( "change" );
+
+
+function stepper(stepper) {
+    var $stepper = $(stepper);
+    var $input = $stepper.siblings('input');
+    var value = parseInt($input.val());
+    if ($stepper.text() == 'up') {
+        if (value < parseInt($input.attr('max'))) {
+            value = value + 1;
+            $input.val(value);
+        }
+    } else {
+        if (value > parseInt($input.attr('min'))) {
+            value = value - 1;
+            $input.val(value);
+        }
+    }
+}
